7ae7552f0a33db9a9e03bff2cdc79a46be5e33e3
Dair Smith <dair.code@gmail.com>
2021-05-19 02:41:15 -0400

    set up form, errors in handlesubmit and onChange

--- src/components/CreateStoryRoute.tsx
+++ src/components/CreateStoryRoute.tsx
@@ -1,9 +1,65 @@
-import React, {useContext} from 'react';
-import {Link} from 'react-router-dom';
-import {StoryContext} from '../context/story-context';
+import React, { useContext, useState, FormEvent } from 'react';
+import { Link } from 'react-router-dom';
+import { StoryContext } from '../context/story-context';
+import { Story } from '../model/Story';
+import { useHistory } from 'react-router';
+import { title } from 'process';
 
 export function CreateStoryRoute() {
+    const { addStory } = useContext(StoryContext);
+    const history = useHistory();
 
+    const [author, setAuthor] = useState("");
+  const [words, setWords] = useState([]);
+
+  function handleSubmit(e: FormEvent) {
+      e.preventDefault();
+
+      const story: Story = {
+          title: title,
+          words: words[]
+      };
+
+      addStory(story);
+      setAuthor("");
+      setWords([]);
+
+      history.push('/');
+  }
+
+  return (
+      <form className="StoryForm" onSubmit={handleSubmit}>
+          <div>
+              <label>Your Name:{" "}
+                  <input type="text" onChange={(e) => setAuthor(e.target.value)}/>
+              </label>
+          </div>
+          <div>
+              <p>Words</p>
+              <div>
+                  <label>person:{" "}
+                  <input type="text" onChange={(e) => setWords([...words, word])}/>
+                  </label>
+              </div>
+              <div>
+                  <label>place:{" "}
+                  <input type="text" onChange={(e) => setWords([...words, word])}/>
+                  </label>
+              </div>
+              <div>
+                  <label>object:{" "}
+                  <input type="text" onChange={(e) => setWords([...words, word])}/>
+                  </label>
+              </div>
+              <div>
+                  <label>object:{" "}
+                  <input type="text" onChange={(e) => setWords([...words, word])}/>
+                  </label>
+              </div>
+          </div>
+          <button type="submit">Save &amp; View Story</button>
+      </form>
+  )
 }
 
 export default CreateStoryRoute;
